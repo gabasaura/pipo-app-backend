b8dd48848e90fd9deb1ef390b716c4f8fbbf6511
gabasaura <gabriela.garin@gmail.com>
2024-07-03 03:41:50 -0400

    deploy for render

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ load_dotenv()
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 
 db.init_app(app)
